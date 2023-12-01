822b0dead3ca298a8e89fd4c50618944f07ca76d
Qixiong Yuan <thisdk@gmail.com>
2023-12-01 14:31:38 +0800

    Update amazon_archlinux_install.sh

--- amazon_archlinux_install.sh
+++ amazon_archlinux_install.sh
@@ -6,11 +6,21 @@ modprobe tcp_bbr
 
 echo "tcp_bbr" > /etc/modules-load.d/80-bbr.conf
 
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.d/30-ipforward.conf
+
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.d/80-bbr.conf
 
-echo "net.core.default_qdisc=fq_codel" >> /etc/sysctl.d/80-bbr.conf
+echo "net.core.default_qdisc=cake" >> /etc/sysctl.d/80-bbr.conf
 
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.d/30-ipforward.conf
+echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.d/99-sysctl.conf
+
+echo "net.ipv4.tcp_rmem = 4096 1048576 4194304" >> /etc/sysctl.d/99-sysctl.conf
+
+echo "net.ipv4.tcp_wmem = 4096 65536 33554432" >> /etc/sysctl.d/99-sysctl.conf
+
+echo "net.ipv4.udp_rmem_min = 8192" >> /etc/sysctl.d/99-sysctl.conf
+
+echo "net.ipv4.udp_wmem_min = 8192" >> /etc/sysctl.d/99-sysctl.conf
 
 echo "net.core.somaxconn = 8192" >> /etc/sysctl.d/99-sysctl.conf
 
@@ -24,19 +34,20 @@ echo "net.core.wmem_max = 33554432" >> /etc/sysctl.d/99-sysctl.conf
 
 echo "net.core.optmem_max = 65536" >> /etc/sysctl.d/99-sysctl.conf
 
-echo "net.ipv4.tcp_rmem = 4096 1048576 4194304" >> /etc/sysctl.d/99-sysctl.conf
-
-echo "net.ipv4.tcp_wmem = 4096 65536 33554432" >> /etc/sysctl.d/99-sysctl.conf
-
-echo "net.ipv4.udp_rmem_min = 8192" >> /etc/sysctl.d/99-sysctl.conf
-
-echo "net.ipv4.udp_wmem_min = 8192" >> /etc/sysctl.d/99-sysctl.conf
-
-echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.d/99-sysctl.conf
-
 echo "net.core.netdev_max_backlog = 16384" >> /etc/sysctl.d/99-sysctl.conf
 
-pacman -S --noconfirm docker
+timedatectl set-ntp true
+
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+hwclock --systohc
+
+mkdir /etc/docker
+
+wget https://raw.githubusercontent.com/thisdk/script/main/daemon.json -O /etc/docker/daemon.json
+
+pacman -S --noconfirm base-devel docker unzip
 
 systemctl enable --now docker
 
+docker network create --driver bridge jason
