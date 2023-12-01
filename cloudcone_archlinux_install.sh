fd5636124346fe040a0c4a88733196e9d55fec1e
Qixiong Yuan <thisdk@gmail.com>
2023-12-01 13:49:57 +0800

    Update cloudcone_archlinux_install.sh

--- cloudcone_archlinux_install.sh
+++ cloudcone_archlinux_install.sh
@@ -6,15 +6,25 @@ modprobe tcp_bbr
 
 echo "tcp_bbr" > /etc/modules-load.d/80-bbr.conf
 
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.d/80-bbr.conf
-
-echo "net.core.default_qdisc=fq_codel" >> /etc/sysctl.d/80-bbr.conf
-
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.d/30-ipforward.conf
 
 echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.d/30-ipforward.conf
 
 echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.d/30-ipforward.conf
+
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.d/80-bbr.conf
+
+echo "net.core.default_qdisc=cake" >> /etc/sysctl.d/80-bbr.conf
+
+echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.d/99-sysctl.conf
+
+echo "net.ipv4.tcp_rmem = 4096 1048576 4194304" >> /etc/sysctl.d/99-sysctl.conf
+
+echo "net.ipv4.tcp_wmem = 4096 65536 33554432" >> /etc/sysctl.d/99-sysctl.conf
+
+echo "net.ipv4.udp_rmem_min = 8192" >> /etc/sysctl.d/99-sysctl.conf
+
+echo "net.ipv4.udp_wmem_min = 8192" >> /etc/sysctl.d/99-sysctl.conf
 
 echo "net.core.somaxconn = 8192" >> /etc/sysctl.d/99-sysctl.conf
 
@@ -27,16 +37,6 @@ echo "net.core.wmem_default = 2097152" >> /etc/sysctl.d/99-sysctl.conf
 echo "net.core.wmem_max = 33554432" >> /etc/sysctl.d/99-sysctl.conf
 
 echo "net.core.optmem_max = 65536" >> /etc/sysctl.d/99-sysctl.conf
-
-echo "net.ipv4.tcp_rmem = 4096 1048576 4194304" >> /etc/sysctl.d/99-sysctl.conf
-
-echo "net.ipv4.tcp_wmem = 4096 65536 33554432" >> /etc/sysctl.d/99-sysctl.conf
-
-echo "net.ipv4.udp_rmem_min = 8192" >> /etc/sysctl.d/99-sysctl.conf
-
-echo "net.ipv4.udp_wmem_min = 8192" >> /etc/sysctl.d/99-sysctl.conf
-
-echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.d/99-sysctl.conf
 
 echo "net.core.netdev_max_backlog = 16384" >> /etc/sysctl.d/99-sysctl.conf
 
